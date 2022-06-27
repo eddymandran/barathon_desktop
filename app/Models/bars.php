d0379b59969edd3d147e008113eb7afa9b8c55b4
eddymandran <eddy.mandran@gmail.com>
2022-06-27 14:53:13 +0200

    generation of migration, model and controller for bars

---
+++ app/Models/bars.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bars extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bars';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'city',
+        'zip_code',
+        'phone',
+        'email',
+        'website',
+        'logo',
+        'cover_picture',
+        'siret',
+        'opening_time',
+        'opening_days',
+        'description',
+        'owner_id',
+    ];
+
+    /**
+     * Get the event associated with the bar
+     */
+    public function event(){
+        return $this->hasMany(Events::class, "bar_id");
+    }
+
+    /**
+     * Get the owner associated with the bar
+     */
+    public function owner(){
+        return $this->belongsTo(User::class, "user_id");
+    }
+}
