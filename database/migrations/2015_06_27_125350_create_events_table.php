df3c8d37ffdd49a2a53e77f6e9664f176a93b5fd
eddymandran <eddy.mandran@gmail.com>
2022-06-27 15:00:16 +0200

    generation of migration, model and controller for filters

--- database/migrations/2015_06_27_125350_create_events_table.php
+++ database/migrations/2015_06_27_125350_create_events_table.php
@@ -14,7 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->id();
+            $ $table->id();
+            $table->dateTime("start_date");
+            $table->dateTime("end_date");
+            $table->text("description")->nullable();
+            $table->integer("number_user_min")->nullable();
+            $table->integer("number_user_max");
+            $table->string("event_picture")->nullable();
+            $table->foreignId("bar_id");
+            $table->foreign("bar_id")->references("id")->on("bars");
             $table->timestamps();
         });
     }
